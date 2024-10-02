594665ad373633519e59882d9cfd372689d12ed2
vivek1842 <vivekghule2002@gmail.com>
2024-10-02 23:11:17 +0530

    functionality added

--- src/App.js
+++ src/App.js
@@ -1,35 +1,73 @@
 import './App.css';
-
+import './index.css'
+import React, {useState} from 'react'
+ 
 function App() {
+ 
+  // state
+  const [weight, setWeight] = useState(0)
+  const [height, setHeight] = useState(0)
+  const [bmi, setBmi] = useState('')
+  const [message, setMessage] = useState('')
+ 
+  let calcBmi = (event) => {
+    //prevent submitting to the server
+    event.preventDefault()
+ 
+    if (weight === 0 || height === 0) {
+      alert('Please enter a valid weight and height')
+    } else {
+      let bmi = (weight / (height/100 * height/100))
+      setBmi(bmi.toFixed(1))
+ 
+      // Logic for message
+ 
+      if (bmi < 18.5) {
+        setMessage('You are underweight')
+      } else if (bmi >= 18.5 && bmi < 24.9) {
+        setMessage('You are a healthy weight')
+      } else if (bmi >= 30 && bmi < 39.9) {
+        setMessage('You are an overweight')
+      } else {
+        setMessage('You are obese')
+      }
+    }
+  }
+ 
+ 
+  let reload = () => {
+    window.location.reload()
+  }
+ 
   return (
     <div className="app">
     <div className='container'>
       <h2 className='center'>BMI Calculator</h2>
-      <form >
+      <form onSubmit={calcBmi}>
  
         <div>
-          <label>Weight (lbs)</label>
-          <input  />
+          <label>Weight (kg)</label>
+          <input value={weight} onChange={(e) => setWeight(e.target.value)} />
         </div>
  
         <div>
-          <label>Height (in)</label>
-          <input  />
+          <label>Height (cm)</label>
+          <input value={height} onChange={(event) => setHeight(event.target.value)} />
         </div>
  
         <div>
           <button className='btn' type='submit'>Submit</button>
-          <button className='btn btn-outline' type='submit'>Reload</button>
+          <button className='btn btn-outline' onClick={reload} type='submit'>Reload</button>
         </div>
       </form>
  
       <div className='center'>
-        <h3>Your BMI is: </h3>
-        <p>message</p>
+        <h3>Your BMI is: {bmi}</h3>
+        <p>{message}</p>
       </div>
     </div>
   </div>
   );
 }
-
+ 
 export default App;
