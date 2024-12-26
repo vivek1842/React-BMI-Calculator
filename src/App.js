1096db92b32ae9dfccc283b94757ad63693edc32
vivek1842 <vivekghule2002@gmail.com>
2024-12-26 23:13:00 +0530

    bmi input form and result integrated

--- src/App.js
+++ src/App.js
@@ -1,73 +1,62 @@
-import './App.css';
-import './index.css'
-import React, {useState} from 'react'
- 
+import "./App.css";
+import "./index.css";
+import React, { useState } from "react";
+import InputForm from "./InputForm";
+import BmiResult from "./BmiResult";
+
 function App() {
- 
   // state
-  const [weight, setWeight] = useState(0)
-  const [height, setHeight] = useState(0)
-  const [bmi, setBmi] = useState('')
-  const [message, setMessage] = useState('')
- 
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [bmi, setBmi] = useState("");
+  const [message, setMessage] = useState("");
+
   let calcBmi = (event) => {
     //prevent submitting to the server
-    event.preventDefault()
- 
+    event.preventDefault();
+
     if (weight === 0 || height === 0) {
-      alert('Please enter a valid weight and height')
+      alert("Please enter a valid weight and height");
     } else {
-      let bmi = (weight / (height/100 * height/100))
-      setBmi(bmi.toFixed(1))
- 
+      let bmi = weight / (((height / 100) * height) / 100);
+      setBmi(bmi.toFixed(1));
+
       // Logic for message
- 
+
       if (bmi < 18.5) {
-        setMessage('You are underweight')
+        setMessage("You are underweight");
       } else if (bmi >= 18.5 && bmi < 24.9) {
-        setMessage('You are a healthy weight')
+        setMessage("You are a healthy weight");
       } else if (bmi >= 30 && bmi < 39.9) {
-        setMessage('You are an overweight')
+        setMessage("You are an overweight");
       } else {
-        setMessage('You are obese')
+        setMessage("You are obese");
       }
     }
-  }
- 
- 
+  };
+
   let reload = () => {
-    window.location.reload()
-  }
- 
+    window.location.reload();
+  };
+
   return (
     <div className="app">
-    <div className='container'>
-      <h2 className='center'>BMI Calculator</h2>
-      <form onSubmit={calcBmi}>
- 
-        <div>
-          <label>Weight (kg)</label>
-          <input value={weight} onChange={(e) => setWeight(e.target.value)} />
-        </div>
- 
-        <div>
-          <label>Height (cm)</label>
-          <input value={height} onChange={(event) => setHeight(event.target.value)} />
-        </div>
- 
-        <div>
-          <button className='btn' type='submit'>Submit</button>
-          <button className='btn btn-outline' onClick={reload} type='submit'>Reload</button>
-        </div>
-      </form>
- 
-      <div className='center'>
-        <h3>Your BMI is: {bmi}</h3>
-        <p>{message}</p>
+      <div className="container">
+        <h2 className="center">BMI Calculator</h2>
+
+        <InputForm
+          weight={weight}
+          height={height}
+          setWeight={setWeight}
+          setHeight={setHeight}
+          calcBmi={calcBmi}
+          reload={reload}
+        />
+
+        <BmiResult bmi={bmi} message={message} />
       </div>
     </div>
-  </div>
   );
 }
- 
+
 export default App;
